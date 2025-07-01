32e0d3997b3fb6edc2c9743131e9c5f6655028b8
beandao <hubin858130@163.com>
2025-07-01 16:15:34 +0800

    修复配置的持久化存储

--- eslint-rules/index.js
+++ eslint-rules/index.js
@@ -1,19 +1,13 @@
 // eslint-rules/index.js
 const noProtobufObjectLiterals = require("./no-protobuf-object-literals")
 const noGrpcClientObjectLiterals = require("./no-grpc-client-object-literals")
-<<<<<<< HEAD
 const noVscodePostmessage = require("./no-vscode-postmessage")
-=======
->>>>>>> 16bc1c863785d2e3350bd9c2baa4bc31be43087d
 
 module.exports = {
 	rules: {
 		"no-protobuf-object-literals": noProtobufObjectLiterals,
 		"no-grpc-client-object-literals": noGrpcClientObjectLiterals,
-<<<<<<< HEAD
 		"no-vscode-postmessage": noVscodePostmessage,
-=======
->>>>>>> 16bc1c863785d2e3350bd9c2baa4bc31be43087d
 	},
 	configs: {
 		recommended: {
@@ -21,10 +15,7 @@ module.exports = {
 			rules: {
 				"local/no-protobuf-object-literals": "error",
 				"local/no-grpc-client-object-literals": "error",
-<<<<<<< HEAD
 				"local/no-vscode-postmessage": "error",
-=======
->>>>>>> 16bc1c863785d2e3350bd9c2baa4bc31be43087d
 			},
 		},
 	},
